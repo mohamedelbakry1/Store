ed3e817ed0d1dcf86fb8c194349ccfdb34d9872b
Mohamed Elbakary <albqrym58@gmail.com>
2025-11-08 20:25:01 +0200

    edit

---
+++ Core/Store.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.Orders
+{
+    // Table
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = 
+    }
+}
